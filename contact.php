dce5afcac20ca3758fc32a8e68d91b3826ed44e0
Gabriel Lima <hdbiel@live.com>
2016-03-04 18:11:56 -0300

    Alterando os campos do formulario de contato

--- contact.php
+++ contact.php
@@ -2,93 +2,93 @@
 
 if(isset($_POST['email'])) {
 
-    $email_to = "joash.pereira@gmail.com";
- 
-    $email_subject = "Website Contact";
+    $email_to = "gdg-duque-de-caxias@googlegroups.com";
 
- 
+    $email_subject = "Contato pelo Site no GitHub";
+
+
     function died($error) {
- 
+
         // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
+
+        echo "Desculpe, mas foram encontrado erros no seu envio. ";
+
+        echo "O erros vão aparecer aqui abaixo.<br /><br />";
+
         echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
+
+        echo "Por favor, conserte-os. =)<br /><br />";
+
         die();
- 
+
     }
- 
-     
- 
+
+
+
     // validation expected data exists
- 
+
     if(!isset($_POST['name']) ||
- 
+
         !isset($_POST['email']) ||
- 
+
         !isset($_POST['message'])) {
- 
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
- 
+
+        died('Desculpe, mas ocorreu um erro durante seu envio.');
+
     }
- 
-     
+
+
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
- 
- 
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
-     
- 
-    $email_message .= "Name: ".clean_string($name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email)."\n";
- 
-    $email_message .= "Message: ".clean_string($message)."\n";
 
- 
+
+    $email_message = "Detalhes do formulário abaixo.\n\n";
+
+
+
+    function clean_string($string) {
+
+      $bad = array("content-type","bcc:","to:","cc:","href");
+
+      return str_replace($bad,"",$string);
+
+    }
+
+
+
+    $email_message .= "Nome: ".clean_string($name)."\n";
+
+    $email_message .= "Email: ".clean_string($email)."\n";
+
+    $email_message .= "Mensagem: ".clean_string($message)."\n";
+
+
 	// create email headers
-	 
+
 	$headers = 'From: '.$email."\r\n".
-	 
+
 	'Reply-To: '.$email."\r\n" .
-	 
+
 	'X-Mailer: PHP/' . phpversion();
-	 
-	@mail($email_to, $email_subject, $email_message, $headers);  
+
+	@mail($email_to, $email_subject, $email_message, $headers);
 
 
 ?>
- 
- 
- 
+
+
+
 <!-- include your own success html here -->
- 
- 
- 
-Thank you for contacting me. Will be in touch with you very soon.
- 
- 
- 
+
+
+
+Muito obrigado por entrar em contato conosco. Assim que pudermos, iremos te retornar.
+
+
+
 <?php
- 
+
 }
- 
+
 ?>
